2c22f5b032985a38a4878c67c099e168f32eb026
Christal Chen <christalchen1209@gmail.com>
2023-05-16 17:16:18 +0800

    feat: fixing heroku seed

--- AC_Project/seeders/20230503133359-users-seed-file.js
+++ AC_Project/seeders/20230503133359-users-seed-file.js
@@ -2,13 +2,13 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('users', [{
       username: 'acuser',
-      password: '$2b$10$YWGmB8BxKrsJLdtKYH1Zp.y8Oay/VKtxEhG16JY4yv2Q4c/qhJL2a', // 預設密碼為 acuser
+      password: await bcrypt.hash('acuser', 10), // 預設密碼為 acuser
       created_at: new Date(),
       updated_at: new Date()
     }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
